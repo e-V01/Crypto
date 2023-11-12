53d6a2202e7ff0c2974ebc888a61738d287d1ce0
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-11-12 14:06:52 +0200

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Y K on 12.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
