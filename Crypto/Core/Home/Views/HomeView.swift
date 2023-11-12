d64ebcb4608e2b47f172acb2414c0b875c250603
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-11-12 14:16:01 +0200

    Grouped and sorted documents outlook

---
+++ Crypto/Core/Home/Views/HomeView.swift
@@ -0,0 +1,26 @@
+//
+//  HomeView.swift
+//  Crypto
+//
+//  Created by Y K on 12.11.2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                
+                //top movers view
+                
+                //all coins view
+            }
+            .navigationTitle("Live Prices")
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
